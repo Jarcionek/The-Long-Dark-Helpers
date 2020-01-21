ad8007c148051ddba226f9288fd0e3b227f0acc0
Jaroslaw Pawlak <jarcionek@users.noreply.github.com>
2020-01-21 13:53:20 +0000

    added scroll bars

--- src/main/java/uk/co/jpawlak/thelongdark/mapnotes/MapNotes.java
+++ src/main/java/uk/co/jpawlak/thelongdark/mapnotes/MapNotes.java
@@ -4,22 +4,31 @@ import javax.imageio.ImageIO;
 import javax.swing.ImageIcon;
 import javax.swing.JFrame;
 import javax.swing.JLabel;
+import javax.swing.JScrollPane;
 import java.awt.image.BufferedImage;
 import java.io.IOException;
 
 public class MapNotes {
 
+    public static final int SCROLL_SENSITIVITY = 15;
+
     public static void main(String[] args) throws IOException {
         JFrame frame = new JFrame();
 
         BufferedImage image = ImageIO.read(MapNotes.class.getResource("/regionimages/Ravine.png"));
-        JLabel panel = new JLabel(new ImageIcon(image));
+        ImageIcon imageIcon = new ImageIcon(image);
+        JLabel mapLabel = new JLabel(imageIcon);
 
-        frame.setContentPane(panel);
+        JScrollPane scrollPane = new JScrollPane(mapLabel);
+        scrollPane.getVerticalScrollBar().setUnitIncrement(SCROLL_SENSITIVITY);
+        scrollPane.getHorizontalScrollBar().setUnitIncrement(SCROLL_SENSITIVITY);
+
+        frame.setContentPane(scrollPane);
 
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         frame.pack();
         frame.setLocationRelativeTo(null);
         frame.setVisible(true);
     }
+
 }
