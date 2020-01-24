7d1c9a7d444537c33a4c7861521c5bb0b5c27b37
Jaroslaw Pawlak <jarcionek@users.noreply.github.com>
2020-01-24 23:11:04 +0000

    added image icons for markers and notes

--- src/main/java/uk/co/jpawlak/thelongdark/mapnotes/MarkerLabel.java
+++ src/main/java/uk/co/jpawlak/thelongdark/mapnotes/MarkerLabel.java
@@ -1,23 +1,34 @@
 package uk.co.jpawlak.thelongdark.mapnotes;
 
+import javax.swing.ImageIcon;
 import javax.swing.JLabel;
-import java.awt.Color;
+import java.net.MalformedURLException;
+import java.net.URISyntaxException;
 
 public class MarkerLabel extends JLabel {
 
     public MarkerLabel(Marker marker) {
-        setSize(20, 20);
-        setOpaque(true);
-        setBackground(color(marker));
+        ImageIcon icon = icon(path(marker));
+        setSize(icon.getIconWidth(), icon.getIconHeight());
+        setIcon(icon);
+        setOpaque(false);
     }
 
-    private Color color(Marker marker) {
+    private String path(Marker marker) {
         switch (marker.getType()) {
-            case TICK: return Color.GREEN;
-            case WARNING: return Color.YELLOW;
-            case UNKNOWN: return Color.BLUE;
-            case CROSS: return Color.RED;
-            default: throw new UnsupportedOperationException("No color defined for type " + marker.getType());
+            case TICK: return "/icons/Tick.png";
+            case WARNING: return "/icons/Exclamation mark.png";
+            case UNKNOWN: return "/icons/Question mark.png";
+            case CROSS: return "/icons/Cross.png";
+            default: throw new UnsupportedOperationException("No image defined for type " + marker.getType());
+        }
+    }
+
+    private ImageIcon icon(String path) {
+        try {
+            return new ImageIcon(getClass().getResource(path).toURI().toURL());
+        } catch (MalformedURLException | URISyntaxException e) {
+            throw new RuntimeException(e);
         }
     }
 
--- src/main/java/uk/co/jpawlak/thelongdark/mapnotes/NoteLabel.java
+++ src/main/java/uk/co/jpawlak/thelongdark/mapnotes/NoteLabel.java
@@ -1,14 +1,25 @@
 package uk.co.jpawlak.thelongdark.mapnotes;
 
+import javax.swing.ImageIcon;
 import javax.swing.JLabel;
-import java.awt.Color;
+import java.net.MalformedURLException;
+import java.net.URISyntaxException;
 
 public class NoteLabel extends JLabel {
 
     public NoteLabel(Note note) {
-        setSize(20, 20);
-        setOpaque(true);
-        setBackground(Color.GRAY);
+        ImageIcon icon = icon();
+        setSize(icon.getIconWidth(), icon.getIconHeight());
+        setIcon(icon);
+        setOpaque(false);
+    }
+
+    private ImageIcon icon() {
+        try {
+            return new ImageIcon(getClass().getResource("/icons/Note.png").toURI().toURL());
+        } catch (MalformedURLException | URISyntaxException e) {
+            throw new RuntimeException(e);
+        }
     }
 
 }
