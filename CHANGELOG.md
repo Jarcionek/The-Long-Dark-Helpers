d97c37262e564c2c46b9ccb03d34846b3d1c1acb
Jaroslaw Pawlak <jarcionek@users.noreply.github.com>
2020-01-24 23:24:26 +0000

    added changelog

---
+++ CHANGELOG.md
@@ -0,0 +1,7 @@
+##### 1.1 (not yet released)
+
+* added image icons for markers
+
+##### 1.0 (21/01/2020)
+
+Initial release
--- release.sh
+++ release.sh
@@ -9,7 +9,8 @@ read version
 
 # UPDATE VERSIONS
 sed -i "s|^    <version>.*</version>|    <version>${version}</version>|" pom.xml
-git add pom.xml
+sed -i "1s|^.*$|##### ${version} (`date +%d/%m/%Y`)|" CHANGELOG.md
+git add pom.xml CHANGELOG.md
 git commit -m "prepare release ${version}"
 git tag "${version}"
 
@@ -20,7 +21,8 @@ mvn package
 # PREPARE FOR NEXT ITERATION
 nextVersion=$((${version%%.*} + 1)).0
 sed -i "s|^    <version>.*</version>|    <version>${nextVersion}-SNAPSHOT</version>|" pom.xml
-git add pom.xml
+sed -i "1s|^|##### ${nextVersion} (not yet released)\n\n|" CHANGELOG.md
+git add pom.xml CHANGELOG.md
 git commit -m "prepare for next development iteration"
 
 # GIT PUSH
